711b57517e2d752f60f4cd60d5b8139d3ab6907f
Justin Looi <justinlooijw@gmail.com>
2023-08-26 04:15:09 +0800

    production 1

--- index.js
+++ index.js
@@ -11,51 +11,40 @@ const {
 } = require("./controller/commandHandlers");
 const { handleInlineQuery } = require("./controller/inlineQueryHandlers");
 
-// Constants
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const PORT = process.env.PORT || 5000;
 
-// const app = express();
+const app = express();
+app.use(bodyParser.json());
 
-// Middleware to parse the updates received from Telegram
-// app.use(bodyParser.json());
+const bot = new TelegramBot(BOT_TOKEN);
+bot.setWebHook(`${process.env.WEBHOOK_URL}/bot${BOT_TOKEN}`);
 
-// Initialize bot and connect to database
-function initializeBot() {
-  connectDB();
+connectDB();
 
-  const bot = new TelegramBot(BOT_TOKEN, { polling: true });
-  console.log("Bot is running...");
-
-  registerBotEvents(bot);
-}
-
-function registerBotEvents(bot) {
+function registerBotEvents() {
   bot.onText(/\/start/, handleStartCommand(bot));
   bot.onText(/\/id (.+)/, handleIdCommand(bot));
   bot.on("message", handleMessageCommand(bot));
-
-  // Inline Query for Student ID
-  // bot.on("inline_query", handleInlineQuery(bot));
-
   bot.onText(/\/(?!start|id\s)(.+)/, handleUnknownCommand(bot));
-
-  // This order ensures that the specific command handlers are checked first, and the generic ones later.
 }
+
+registerBotEvents();
 
 process.on("unhandledRejection", (error) => {
   console.error("unhandledRejection", error);
 });
 
-// Start the bot
-initializeBot();
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-// app.get(`/`, (req, res) => {
-  
-//   res.send("hello world");
-// });
+app.post(`/bot${BOT_TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
 
-// // Start the Express server
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log("Bot is running with webhook setup...");
+});
