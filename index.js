e3a15477efbb33662ad4ab90b98718eb16820e1d
Justin Looi <justinlooijw@gmail.com>
2023-08-27 01:00:36 +0800

    Production fix 2

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const bodyParser = require("body-parser");
 const axios = require("axios");
 const connectDB = require("./db");
 const {
-    handleStartCommand,
-    handleIdCommand,
-    handleMessageCommand,
-    handleUnknownCommand,
+  handleStartCommand,
+  handleIdCommand,
+  handleMessageCommand,
+  handleUnknownCommand,
 } = require("./controller/commandHandlers");
 const { handleInlineQuery } = require("./controller/inlineQueryHandlers");
 
@@ -27,29 +27,29 @@ bot.setWebHook(WEBHOOK_URL);
 connectDB();
 
 function registerBotEvents() {
-    bot.onText(/\/start/, handleStartCommand(bot));
-    bot.onText(/\/id (.+)/, handleIdCommand(bot));
-    bot.on("message", handleMessageCommand(bot));
-    bot.onText(/\/(?!start|id\s)(.+)/, handleUnknownCommand(bot));
+  bot.onText(/\/start/, handleStartCommand(bot));
+  bot.onText(/\/id (.+)/, handleIdCommand(bot));
+  bot.on("message", handleMessageCommand(bot));
+  bot.onText(/\/(?!start|id\s)(.+)/, handleUnknownCommand(bot));
 }
 
 registerBotEvents();
 
 process.on("unhandledRejection", (error) => {
-    console.error("unhandledRejection", error);
+  console.error("unhandledRejection", error);
 });
 
 app.get("/", (req, res) => {
-    res.send("hello world");
+  res.send("hello world");
 });
 
 // Handling updates from Telegram via the webhook route
 app.post(WEBHOOK_URI, (req, res) => {
-    bot.processUpdate(req.body); // Let the bot process the update
-    res.sendStatus(200); // Responding with 200 to acknowledge receipt of the update
+  bot.processUpdate(req.body); // Let the bot process the update
+  res.status(200).send("ok"); // Responding with 200 to acknowledge receipt of the update
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    console.log(`Bot is running with webhook at: ${WEBHOOK_URL}`);
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Bot is running with webhook at: ${WEBHOOK_URL}`);
 });
