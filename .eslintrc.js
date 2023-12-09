562d3c6d97578c197316bbec1551854692b53656
Justin Looi <justinlooijw@gmail.com>
2023-12-09 14:36:11 +0800

    Update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,33 @@
 module.exports = {
   env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
+    commonjs: true, // Enable CommonJS global variables and CommonJS scoping (useful for Node.js)
+    es2021: true, // Enable all ECMAScript 2021 features
+    node: true, // Enable Node.js global variables and Node.js scoping
   },
   extends: [
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-    "plugin:jest/recommended",
+    "eslint:recommended", // Use the recommended rules from ESLint
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors
+    "plugin:jest/recommended", // Use Jest recommended rules
   ],
-  plugins: ["prettier", "jest"],
+  plugins: [
+    "prettier", // Allows for linting of Prettier formatting issues
+    "jest", // Support for Jest testing framework
+  ],
   overrides: [
     {
       env: {
-        node: true,
+        node: true, // Override: Explicitly enable Node.js environment
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: [".eslintrc.{js,cjs}"], // Target specific files for this override
       parserOptions: {
-        sourceType: "script",
+        sourceType: "script", // Treat files as CommonJS (as opposed to ES modules)
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: "latest", // Use the latest ECMAScript standard
   },
-  rules: { "prettier/prettier": ["warn", { endOfLine: "auto" }] },
+  rules: {
+    "prettier/prettier": ["warn", { endOfLine: "auto" }], // Warn on prettier issues, override end-of-line setting
+  },
 };
